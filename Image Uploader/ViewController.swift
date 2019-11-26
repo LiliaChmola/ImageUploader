6268b0d6d4e59621d1502155065847ba484f1127
Lilia Chmola <chmola.lilia@gmail.com>
2019-11-26 15:35:34 +0200

    - Initial commit

---
+++ Image Uploader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Uploader
+//
+//  Created by Chmola Lilia on 11/26/19.
+//  Copyright © 2019 Lilia Chmola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
